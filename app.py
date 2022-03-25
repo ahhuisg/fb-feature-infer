b16bf0b0b82c8cea998da870d863deda198abae3
YH Chen <yhchen@softgrass.com>
2022-03-25 15:16:39 +0800

    DMV implementation

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
+import subprocess
+import traceback
 import joblib
+from datetime import datetime
 
 from flask import Flask, render_template, request
 import pandas as pd
 import shap
+import logzero
+from logzero import logger
+
+logzero.logfile(os.path.join(os.path.dirname(__file__), "fb.log"))
 
 app = Flask(__name__,
             static_url_path='',
@@ -34,6 +41,7 @@ column_names = model.fe_columns
 column_names.insert(0, "Column Name")
 column_names.insert(1, "Predicted Column Type")
 column_names.insert(2, "Prediction Probability")
+column_names.insert(3, "Disguised Missing Value (DMV)")
 column_names.append("attribute_name")
 
 
@@ -44,7 +52,6 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    #new_column_names = copy.deepcopy(column_names)
 
     items = []
     if request.method == 'POST':
@@ -53,6 +60,7 @@ def upload_file():
             shap_flag = True
 
         file = request.files["file"]
+        logger.info(f'Start uploading {file.filename}')
         df = pd.read_csv(file)
 
         attr_names = df.columns
@@ -62,7 +70,7 @@ def upload_file():
 
         for ind, attr in enumerate(attr_names):
             percent = round(y_rf_probs[ind][y_rf[ind]], 3)
-            d = {'name': attr, 'type': dict_int_label[y_rf[ind]], 'prob': percent}
+            d = {'name': attr, 'type': dict_int_label[y_rf[ind]], 'prob': percent, 'dmv': ''}
             f_dict = data_featurized.iloc[ind][:25].to_dict()
 
             if shap_flag:
@@ -72,11 +80,55 @@ def upload_file():
                     f_dict[key] = f'{f_dict[key]} (SHAP Value: {shap_values[i]})'
 
                 f_dict['attribute_name'] = f'(SHAP Value: {sum(all_shap_values[y_rf[ind]][25:])})'
+
+                logger.info(f'Done process SHAP value for {attr}')
             else:
                 f_dict['attribute_name'] = ""
 
             d.update(f_dict)
             items.append(d)
+
+        file_name = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
+        upload_path = f'upload/{file_name}'
+        logger.info(f'Uploading file to {upload_path}')
+        df.to_csv(upload_path, index=False)
+        logger.info(f'Start processing DMV FOR {file_name}')
+        dmv_df_list = []
+        for dir_name, type_name in [('syntactic', '1'), ('numerical', '3')]:
+            command = ['./FAHES', upload_path, f'dmv/{dir_name}', type_name]
+            process = subprocess.Popen(command,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            try:
+                stdout, stderr = process.communicate(timeout=30)
+                logger.info(f'Done {dir_name}')
+                logger.debug(f'stdout: {stdout}')
+                logger.debug(f'stderr: {stderr}')
+                dmv_df_list.append(pd.read_csv(f'dmv/{dir_name}/DMV_{file_name}'))
+            except:
+                logger.error(f'Error with {dir_name}: {traceback.format_exc()}')
+
+        dmv_df = pd.concat(dmv_df_list)
+
+        for d in items:
+            attr_name = d['name']
+            dmvs = dmv_df[dmv_df['Attribute Name'] == f'{attr_name}']
+            if len(dmvs) > 0:
+                for i in range(len(dmvs)):
+                    if i == 0:
+                        d['dmv'] += '['
+
+                    d_v = dmvs.iloc[i]
+                    d_type = 'Syntactic-Outlier' if d_v['Detecting Tool'] == 'SYN' else 'Numeric-Outlier'
+                    d['dmv'] += f'(DMV: {d_v["DMV"]}; Type: {d_type}; Frequency: {d_v["Frequency"]})'
+
+                    if i < len(dmvs) - 1:
+                        d['dmv'] += ','
+
+                    if i == len(dmvs) - 1:
+                        d['dmv'] += ']'
+
+        logger.info(f'Done processing DMV FOR {file_name}')
 
     return render_template("index.html", column_names=column_names, items=items)
 
